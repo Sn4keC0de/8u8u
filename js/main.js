1dec3153f4f48ac32327788b1f5a0ce672e22630
SnakeCode <kevindarenas@gmail.com>
2025-01-18 02:51:29 -0300

    Modificado 1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 const code = document.getElementById('code');
 
 code.onclick = function () {
-    document.execCommand(copy);
+    document.execCommand('copy');
 }
 
 code.addEventListener('copy', function(event) {
     event.preventDefault();
     if (event.clipboardData) {
         event.clipboardData.setData("text/plain", code.textContent);
-        console.log(event.clipboardData.getData("text"));
+        // console.log(event.clipboardData.getData("text"));
     }    
 });
